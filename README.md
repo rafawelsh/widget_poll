d439b99adf8883ea874479f0b742ed813b1ea8a1
Rafael Castellanos-Welsh <mrrafael1912@gmail.com>
2025-08-14 21:28:21 -0400

    initial commit with simple react router

---
+++ README.md
@@ -0,0 +1,5 @@
+# Simple Poll 
+
+This is an exercise to go through the steps outlined by GreatFrontEnd system design problem Widget Poll 
+
+More info soon
---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import App from './App.tsx';
+import Poll from './components/poll/poll.tsx';
+import './index.css';
+
+createRoot(document.getElementById('root')!).render(
+	<BrowserRouter>
+		<Routes>
+			<Route path='/' element={<App />} />
+			<Route path='/poll' element={<Poll />} />
+		</Routes>
+	</BrowserRouter>
+);
